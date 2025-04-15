8a350949c524adf05ba04dedd4138e462d222a5f
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2025-04-15 10:39:13 +0200

    refactor(topology nav): split files

---
+++ scripts/matching/topology_nav_graph.py
@@ -0,0 +1,150 @@
+from __future__ import annotations
+
+import logging
+import pickle
+from pathlib import Path
+from typing import Optional, List, Tuple
+
+import cv2
+import matplotlib
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+from matching.topology_nav_utils import get_xfeat
+from scripts.utils.datasets import get_dataset_by_name
+
+default_path = Path(get_dataset_by_name("norlab_ulaval_datasets/node_dataset"))
+logger = logging.getLogger(__name__)
+
+
+class Node:
+    def __init__(self, name, coordinate: np.ndarray, radius: float) -> None:
+        self.name = name
+        self.coordinate = coordinate
+        self.radius = radius
+        self.path = default_path / self.name
+        self.path.mkdir(parents=True, exist_ok=True)
+        self.patches = []
+        self.correspondance_data = []
+
+    def distance(self, c: np.ndarray) -> float:
+        return float(np.linalg.norm(self.coordinate - c))
+
+    def is_in(self, c: np.ndarray) -> bool:
+        return self.distance(c) < self.radius
+
+    def add_patch(self, image: np.ndarray, c: np.ndarray) -> None:
+        cv2.imwrite(str(self.path / f"{len(self.patches)}.png"), image)
+        self.patches.append((c, str(self.path / f"{len(self.patches)}.png")))
+
+    def save_patches_metadata(self):
+        df = pd.DataFrame([(c[0], c[1], o, path) for (c, o, path) in self.patches], columns=["x", "y", "yaw", "path"])
+        df.to_csv(str(self.path / "metadata.csv"), index=False)
+
+    def load_patches_metadata(self):
+        df = pd.read_csv(str(self.path / "metadata.csv"))
+        self.patches = [(np.array([r['x'], r['y']]), r['yaw'], r['path']) for _, r in df.iterrows()]
+
+    def save_correspondances(self):
+        for i, (_, _, feat) in enumerate(self.correspondance_data):
+            with open(str(self.path / f"feat_{i}.pkl"), "wb") as f:
+                pickle.dump(feat, f)
+        df = pd.DataFrame([(image_path, c[0], c[1], str(self.path / f"feat_{i}.pkl"))
+                           for i, (image_path, c, _) in enumerate(self.correspondance_data)],
+                          columns=["image_path", "x", "y", "feat_path"])
+        df.to_csv(str(self.path / "feat_metadata.csv"), index=False)
+
+    def load_correspondances(self):
+        df = pd.read_csv(str(self.path / "feat_metadata.csv"))
+
+        for _, row in df.iterrows():
+            with open(row["feat_path"], "rb") as f:
+                self.correspondance_data.append((row['image_path'],
+                                                 np.array([row['x'], row['y']]),
+                                                 pickle.load(f)))
+
+    def find_correspondances(self, image: np.ndarray) -> List[Tuple[np.ndarray, np.ndarray]]:
+        xfeat = get_xfeat()
+        ugv_feature = xfeat.detectAndCompute(image, top_k=4096)[0]
+        ugv_feature.update({'image_size': (image.shape[1], image.shape[0])})
+        correspondance = []
+        for img_path, c, uav_feature in self.correspondance_data:
+            mkpts_0, mkpts_1, _ = xfeat.match_lighterglue(ugv_feature, uav_feature)
+            correspondance.append((mkpts_0, mkpts_1))  # TODO check if type is np.ndarray
+        return correspondance
+
+    def __str__(self):
+        return f"Node({self.name})"
+
+    def __repr__(self):
+        return str(self)
+
+
+class Graph:
+    def __init__(self):
+        self.nodes = []
+        self.edges = {}
+
+    def add_node(self, node: Node):
+        """
+        Add a node to the graph
+        :param node: the node to add
+        """
+        self.nodes.append(node)
+        self.edges[node] = []
+
+    def add_edge(self, n1: Node, n2: Node):
+        """
+        Add an edge between two nodes. This is a non-oriented graph.
+        :param n1: the first node
+        :param n2: the second node
+        """
+        if n1 not in self.edges:
+            self.add_node(n1)
+        if n2 not in self.edges[n1]:
+            self.edges[n1].append(n2)
+        if n2 not in self.edges:
+            self.add_node(n2)
+        if n1 not in self.edges[n2]:
+            self.edges[n2].append(n1)
+
+    def plot(self):
+        fig, ax = plt.subplots()
+        ax.add_collection(matplotlib.collections.PatchCollection([plt.Circle(n.coordinate,
+                                                                             radius=n.radius,
+                                                                             linewidth=0) for n in self.nodes],
+                                                                 facecolor='purple'))
+
+        for n1 in self.edges:
+            for n2 in self.edges[n1]:
+                x_values = [n1.coordinate[0], n2.coordinate[0]]
+                y_values = [n1.coordinate[1], n2.coordinate[1]]
+                ax.plot(x_values, y_values, color='black')  # Ensure color is set for visibility
+
+        return fig
+
+    def get_current_node(self, c: np.ndarray) -> Optional[Node]:
+        for n in self.nodes:
+            if n.is_in(c):
+                return n
+
+    def save(self):
+        path = default_path / "graph.csv"
+        df = pd.DataFrame([(n.name, n.coordinate[0], n.coordinate[1], n.radius,
+                            ",".join([m.name for m in self.edges[n]])) for n in self.nodes],
+                          columns=["name", "x", "y", "r", "connected"])
+        df.to_csv(path, index=False)
+
+    def load(self):
+        path = default_path / "graph.csv"
+        df = pd.read_csv(str(path))
+
+        constructed_node = {}
+        for _, row in df.iterrows():
+            constructed_node[str(row["name"])] = Node(str(row["name"]), (row["x"], row["y"]), row["r"])
+            self.add_node(constructed_node[str(row["name"])])
+
+        for _, row in df.iterrows():
+            for connected_node_name in row["connected"].split(","):
+                self.add_edge(constructed_node[str(row["name"])], constructed_node[connected_node_name])
---
+++ scripts/matching/topology_nav_utils.py
@@ -0,0 +1,92 @@
+import logging
+import os
+from typing import Optional, Tuple, List
+
+import cv2
+import numpy as np
+import pandas as pd
+import torch
+
+from matching.topology_nav_graph import Graph, Node
+
+_initiated_xfeat: Optional[torch] = None
+logger = logging.getLogger(__name__)
+
+
+def init_xfeat():
+    global _initiated_xfeat
+    os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+    logger.debug(f"Cuda device(s) {os.environ['CUDA_VISIBLE_DEVICES']}"
+                 if "CUDA_VISIBLE_DEVICES" in os.environ else "No CUDA devices")
+    _initiated_xfeat = torch.hub.load('verlab/accelerated_features', 'XFeat', pretrained=True, top_k=4096)
+
+
+def get_xfeat():
+    global _initiated_xfeat
+    if _initiated_xfeat is None:
+        init_xfeat()
+    return _initiated_xfeat
+
+
+def overlap(rect1: np.ndarray, rect2: np.ndarray) -> bool:
+    for p in rect1:
+        if cv2.pointPolygonTest(rect2.reshape(-1, 1, 2), p.astype(np.float32), False) >= 0:
+            return True
+    for q in rect2:
+        if cv2.pointPolygonTest(rect1.reshape(-1, 1, 2), q.astype(np.float32), False) >= 0:
+            return True
+    if cv2.pointPolygonTest(rect1, rect2.mean(axis=0).astype(np.float32), False) >= 0:
+        return True
+    return False
+
+
+def rotation_matrix(a: float) -> np.ndarray:
+    return np.array([[np.cos(a), -np.sin(a)],
+                     [np.sin(a), np.cos(a)]])
+
+
+def get_transformation_matrix(xy: np.ndarray, a: float) -> np.ndarray:
+    t = np.zeros((3, 3))
+    t[:2, :2] = rotation_matrix(a)
+    t[:2, 2] = xy
+    t[2, 2] = 1
+    return t
+
+
+def from_ugv_to_pixel(xy: np.ndarray, aruco_a: float, aruco_c: np.ndarray, pixels_per_meter: float) -> np.array:
+    return (aruco_c - (rotation_matrix(aruco_a) @ xy)[::-1] * pixels_per_meter).astype(np.int32)
+
+
+def from_pixel_to_ugv(ij: np.ndarray, aruco_a: float, aruco_c: np.ndarray, pixel_per_meter: float) -> np.array:
+    return rotation_matrix(-aruco_a) @ ((aruco_c - ij.astype(np.float32)) / pixel_per_meter)[::-1]
+
+
+def get_path_in_node(gnss_norlab: pd.DataFrame, graph: Graph) -> Tuple[List[Node], List[Node]]:
+    path_in_nodes = []
+    left_node = True
+    current_nodes = []
+    for x, y in zip(gnss_norlab['x'], gnss_norlab['y']):
+        n = graph.get_current_node(np.array([x, y]))
+        current_nodes.append(n)
+        if n is None:
+            left_node = True
+        elif left_node:
+            path_in_nodes.append(n)
+            left_node = False
+    logger.info(path_in_nodes)
+
+    # same as current_nodes but none value are replace by the next node the robot will be visiting
+    next_nodes = []
+    next_node_idx = 0
+    next_node_idx_used = False
+    for i in range(len(current_nodes)):
+        if current_nodes[i] is None:
+            next_nodes.append(path_in_nodes[next_node_idx] if next_node_idx < len(path_in_nodes) else None)
+            next_node_idx_used = True
+        else:
+            if next_node_idx_used:
+                next_node_idx += 1
+                next_node_idx_used = False
+            next_nodes.append(current_nodes[i])
+
+    return current_nodes, next_nodes
