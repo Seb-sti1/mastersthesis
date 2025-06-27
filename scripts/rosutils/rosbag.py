64d8f5e624f2560d9ff208aeb45c6d3c9ef417c3
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2025-06-27 14:24:16 +0200

    feat(utils): extract data from rosbag

--- scripts/rosutils/rosbag.py
+++ scripts/rosutils/rosbag.py
@@ -5,6 +5,7 @@ This file is for rosbag manipulations
 from pathlib import Path
 from typing import Callable, Dict, Any, Iterator
 
+import cv2
 import numpy as np
 from cv_bridge import CvBridge
 from tqdm import tqdm
@@ -12,6 +13,32 @@ from tqdm import tqdm
 import rosbag
 
 bridge = CvBridge()
+
+
+def withTimestamp(func):
+    return lambda msg, t: (t, func(msg, t))
+
+
+def msg2gnss(msg, t):
+    return {
+        "latitude": msg.latitude,
+        "longitude": msg.longitude,
+        "altitude": msg.altitude
+    }
+
+
+def msg2alt(msg, t):
+    return msg
+
+
+def msg2gimbalStatus(msg, t):
+    return {
+        "roll": msg.data[0],
+        "pitch": msg.data[1],
+        "yaw": msg.data[2],
+        "manually set zoom": msg.data[3],
+        "zoom": msg.data[4],
+    }
 
 
 def msg2imu(msg, t):
@@ -27,6 +54,9 @@ def msg2imu(msg, t):
 
 
 def msg2image(msg, t):
+    if "CompressedImage" in str(type(msg)):
+        np_arr = np.fromstring(msg.data, np.uint8)
+        return cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
     return bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
 
 
@@ -34,7 +64,7 @@ def msg2calibration(msg, t):
     return np.reshape(msg.K, (3, 3))
 
 
-def replay_bag(bag_file: Path, topics: Dict[str, Callable]) -> Iterator[Dict[str, Any]]:
+def replay_bag(bag_file: Path, topics: Dict[str, Callable], exhaustive: bool = False) -> Iterator[Dict[str, Any]]:
     bag = rosbag.Bag(bag_file)
     found = {t: False for t in topics.keys()}
     last_published = {t: (None, None) for t in topics.keys()}
@@ -43,8 +73,13 @@ def replay_bag(bag_file: Path, topics: Dict[str, Callable]) -> Iterator[Dict[str
         last_published[topic] = (msg, t)
         found[topic] = True
 
-        if all(found.values()):
-            yield {name: topics[name](msg, t) for name, (msg, t) in last_published.items()}
+        if (exhaustive and any(found.values())) or (not exhaustive and all(found.values())):
+            yield {name: topics[name](msg, t) for name, (msg, t) in last_published.items() if msg is not None}
             found = {t: False for t in topics.keys()}
+            if exhaustive:
+                # when it is exhaustive replay there is a need to reset last_published
+                # as otherwise the data will be yielded multiple time
+                last_published = {t: (None, None) for t in topics.keys()}
+
     bag.close()
     return None
